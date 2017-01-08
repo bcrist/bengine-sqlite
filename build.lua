5afc67fe98786c3419076ee062e88dc0a6276369
Ben Crist <ben@magicmoremagic.com>
2017-01-08 15:57:09 -0600

    build.lua syntax overhaul

--- build.lua
+++ build.lua
@@ -1,32 +1,21 @@
-module { name = 'sqlite',
-   projects = {
-      lib {
-         limp = {
-            { file = 'include/transaction_type.hpp', inputs = { 'meta/transaction_type.lua', 'meta/transaction_type_hpp.blt' } },
-            { file = 'src/transaction_type.cpp', inputs = { 'meta/transaction_type.lua', 'meta/transaction_type_cpp.blt' } },
-            { file = 'include/result_type.hpp', inputs = { 'meta/result_type.lua', 'meta/result_type_hpp.blt' } },
-            { file = 'src/result_type.cpp', inputs = { 'meta/result_type.lua', 'meta/result_type_cpp.blt' } }
-         },
-         src = {
-            'src/*.cpp',
-            pch = 'src/pch.cpp',
-            nopch = 'src/*.c'
-         },
-         preprocessor = {
-            'BE_SQLITE_IMPL'
-         }
-      },
-      app { suffix = 'test',
-         icon = 'icon/bengine-test-perf.ico',
-         src = {
-            'test/*.cpp'
-         },
-         libs = {
-            'testing',
-            'core',
-            'core-id',
-            'sqlite'
-         }
+module 'sqlite' {
+   lib {
+      limp { file = 'include/transaction_type.hpp', inputs = { 'meta/transaction_type.lua', 'meta/transaction_type_hpp.blt' } },
+      limp { file = 'src/transaction_type.cpp', inputs = { 'meta/transaction_type.lua', 'meta/transaction_type_cpp.blt' } },
+      limp { file = 'include/result_type.hpp', inputs = { 'meta/result_type.lua', 'meta/result_type_hpp.blt' } },
+      limp { file = 'src/result_type.cpp', inputs = { 'meta/result_type.lua', 'meta/result_type_cpp.blt' } },
+      src_no_pch 'src/*.c',
+      src 'src/*.cpp',
+      pch_src 'src/pch.cpp',
+      define 'BE_SQLITE_IMPL'
+   },
+   app '-test' {
+      icon 'icon/bengine-test-perf.ico',
+      link_project {
+         'testing',
+         'core',
+         'core-id',
+         'sqlite'
       }
    }
 }
