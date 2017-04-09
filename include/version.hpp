6e686dc359a8edb2f97e56b1691eb77cae8153ff
Ben Crist <ben@magicmoremagic.com>
2017-04-09 18:17:40 -0500

    versioning

---
+++ include/version.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef BE_SQLITE_VERSION_HPP_
+#define BE_SQLITE_VERSION_HPP_
+
+#include <be/core/macros.hpp>
+
+#define BE_SQLITE_VERSION_MAJOR 0
+#define BE_SQLITE_VERSION_MINOR 1
+#define BE_SQLITE_VERSION_REV 1
+
+/*!! include('common/version', 'be::sqlite') !! 6 */
+/* ################# !! GENERATED CODE -- DO NOT MODIFY !! ################# */
+#define BE_SQLITE_VERSION (BE_SQLITE_VERSION_MAJOR * 100000 + BE_SQLITE_VERSION_MINOR * 1000 + BE_SQLITE_VERSION_REV)
+#define BE_SQLITE_VERSION_STRING "be::sqlite " BE_STRINGIFY(BE_SQLITE_VERSION_MAJOR) "." BE_STRINGIFY(BE_SQLITE_VERSION_MINOR) "." BE_STRINGIFY(BE_SQLITE_VERSION_REV)
+
+/* ######################### END OF GENERATED CODE ######################### */
+
+#endif
